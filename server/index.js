455298e7f8dd477388dc61fef279164f785f33e1
SahithiDevalla <sahithidsnl@gmail.com>
2024-07-03 22:10:44 +0530

    New

--- server/index.js
+++ server/index.js
@@ -7,10 +7,11 @@ const quizRoutes = require('./routes/quizzes');
 require('dotenv').config();
 
 const app = express();
-const path = require("path");
+const PORT = process.env.PORT || 9000;
 let server;
 
-// Middleware config
+// middlware config
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true, limit: '20mb' }));
 app.use(bodyParser.json({ limit: '20mb' }));
@@ -18,25 +19,12 @@ app.use(bodyParser.json({ limit: '20mb' }));
 app.use('/api/users/', userRoutes);
 app.use('/api/quizzes/', quizRoutes);
 
-// Serve static files from the React app
-app.use(express.static(path.resolve(__dirname, "client", "build")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-
-// Define the PORT variable
-const PORT = process.env.PORT || 5000;
-
-// Connect to MongoDB
 mongoose.connect(process.env.DB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-})
-.then(() => console.log('Database connection established'))
-.catch(err => console.log('Error connecting to mongodb instance: ', err));
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }).then(() => console.log('Database connection established'))
+    .catch(er => console.log('Error connecting to mongodb instance: ', er));
 
-// Start the server
 server = app.listen(PORT, () => {
     console.log(`Node server running on port: ${PORT}`);
 });
