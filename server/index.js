ce82bbd62655774cfbb7c88b4e1568cd64998110
SahithiDevalla <sahithidsnl@gmail.com>
2024-07-03 21:05:17 +0530

    Vercel deployment

--- server/index.js
+++ server/index.js
@@ -7,11 +7,10 @@ const quizRoutes = require('./routes/quizzes');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 9000;
+const path = require("path");
 let server;
 
-// middlware config
-
+// Middleware config
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true, limit: '20mb' }));
 app.use(bodyParser.json({ limit: '20mb' }));
@@ -19,12 +18,25 @@ app.use(bodyParser.json({ limit: '20mb' }));
 app.use('/api/users/', userRoutes);
 app.use('/api/quizzes/', quizRoutes);
 
-mongoose.connect(process.env.DB_URI, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    }).then(() => console.log('Database connection established'))
-    .catch(er => console.log('Error connecting to mongodb instance: ', er));
+// Serve static files from the React app
+app.use(express.static(path.resolve(__dirname, "client", "build")));
 
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+// Define the PORT variable
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.DB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+.then(() => console.log('Database connection established'))
+.catch(err => console.log('Error connecting to mongodb instance: ', err));
+
+// Start the server
 server = app.listen(PORT, () => {
     console.log(`Node server running on port: ${PORT}`);
 });
